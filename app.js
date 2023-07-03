660511dba784ab3d6eab6e4006ec7da5d9173688
acz19 <acz1992@hotmail.co.uk>
2023-07-03 12:20:48 +0200

    Add initial mathmatical functions

---
+++ app.js
@@ -0,0 +1,30 @@
+/* Math functions */
+
+// More complex Add Functions that add more than two numbers
+/* function add(array) {
+    return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    } */
+
+/*     function add(array) {
+        let sum = 0;
+        for(let i = 0; i < array.length; i++) {
+            sum += array[i]
+        }
+        return sum
+    } */
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
