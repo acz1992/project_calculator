a9291e44a72f62a263f036d4c9f01d03bb3bb1d1
acz19 <acz1992@hotmail.co.uk>
2023-07-06 16:46:57 +0200

    Enable operator buttons, prevent screen overflow and double decimal usage

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 A simple Calculator app
 
-Notepad:
+Still to do:
 
+1 - Add Clear and Delete button functionalities
+2 - Allow calculator to string operations from one to another
+3 - Hook up buttons to computer keyboard
+
+Notepad:
 // More complex Add Functions that add more than two numbers
 /_ function add(array) {
 return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
--- app.js
+++ app.js
@@ -23,15 +23,16 @@ function power(a, b) {
 /* Calculator Initialisation */
 
 let num1; // first button that is clicked
-let operator = "add" || "subtract" || "multiply" || "divide" || "power"; // Will hook up to buttons with operator symbols
+/* let operator = "add" || "minus" || "multiply" || "divide" || "power"; */ // Will hook up to buttons with operator symbols
 let num2; // second button that is clicked
+let operator;
 
 function operate(num1, num2, operator) {
 	switch (operator) {
 		case "add":
 			return add(num1, num2);
 			break;
-		case "subtract":
+		case "minus":
 			return subtract(num1, num2);
 			break;
 		case "multiply":
@@ -59,13 +60,6 @@ numBtns.forEach((numBtn) => {
 	});
 });
 
-function updateDisplay(value) {
-	if (bottomDisplayValue.length < 14) {
-		bottomDisplayValue += value;
-	}
-	bottomDisplay.innerHTML = bottomDisplayValue;
-}
-
 /* Top Display */
 
 // Initialize topDisplayValue
@@ -78,24 +72,65 @@ topDisplay.innerHTML = topDisplayValue;
 // Declare variables globally
 let firstInput;
 let secondInput;
+// Keep track whether current input is first or second
+let isFirstInput = true;
 
 // Add event listeners to operation buttons
 const operationBtns = document.querySelectorAll(".operationBtns");
 operationBtns.forEach((operationBtn) => {
 	operationBtn.addEventListener("click", () => {
 		// Save first input and operation
-		firstInput = bottomDisplayValue;
-		operation = operationBtn.id;
+		firstInput = Number(bottomDisplayValue);
+		operator = operationBtn.id;
 		// clear bottom display and add initial value to Top Display
 		topDisplay.innerHTML = firstInput + operationBtn.innerHTML;
 		bottomDisplayValue = "";
 		bottomDisplay.innerHTML = bottomDisplayValue;
+		isFirstInput = false;
 	});
 });
+
+// Maps operator variable IDs to symbols for use in top display
+const operatorSymbols = {
+	add: "&#43",
+	minus: "&#45",
+	multiply: "&#215;",
+	divide: "&#247;",
+};
 
 // Call Operate function when "equals" clicked
 const equals = document.querySelector("#equals");
 equals.addEventListener("click", () => {
-	let solution = operate(firstInput, secondInput, operator);
-	updateDisplay(solution);
+	if (firstInput !== undefined && bottomDisplayValue !== "") {
+		secondInput = Number(bottomDisplayValue);
+		//
+		topDisplay.innerHTML = `${firstInput} ${operatorSymbols[operator]} ${secondInput}`;
+		bottomDisplayValue = "";
+		let solution = operate(firstInput, secondInput, operator);
+		updateDisplay(solution);
+	}
 });
+
+/* Prevent decimal point from being entered more than once */
+
+// Initialise variables trakcing decimal usage
+let firstInputHasDecimal = false;
+let secondInputHasDecimal = false;
+
+function updateDisplay(value) {
+	// ignore input if decimal already present
+	if (value === "." && isFirstInput && firstInputHasDecimal) {
+		return;
+	} else if (value === "." && !isFirstInput && secondInputHasDecimal) {
+		return;
+	}
+	bottomDisplayValue += value.toString();
+	bottomDisplay.innerHTML = bottomDisplayValue;
+
+	// Update decimal usage
+	if (value === "." && isFirstInput) {
+		firstInputHasDecimal = true;
+	} else if ((value === ".") & !isFirstInput) {
+		secondInputHasDecimal = true;
+	}
+}
